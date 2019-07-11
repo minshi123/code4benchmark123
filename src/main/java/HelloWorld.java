16404585b57716fda285c1fb33649e9a012087d9
caprsediemsoul <52734560+supremeGoodness@users.noreply.github.com>
2019-07-11 16:52:22 +0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -10,28 +10,28 @@ public class HelloWorld {
 
     void doesNotCauseNPE(){
         Pointers.A a = Pointers.mayReturnNull(10);
-        a.method();
+        a.method(); // ok
     }
 
     void mayCauseNPE(){
         Pointers.A a = Pointers.mayReturnNull(-10);
-        a.method();
+        a.method(); // defect 1 
     }
 
     void alwaysCauseNPE(){
         String s = null;
-        System.out.println(s.length());
+        System.out.println(s.length()); // defect 2
     }
 
     void forwardInferNPE(String s){
         if (s != null){
             dummy();
         }
-        System.out.println(s.length());
+        System.out.println(s.length()); // defect 3
     }
 
     void reverseInferNPE(String s){
-        System.out.println(s.length());
+        System.out.println(s.length()); // defect 4
         if (s != null){
             dummy();
         }
